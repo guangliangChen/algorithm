92d132af0533f8d773a8741c4d3a5edb2e916739
guangliangChen <guangliangbiz@163.com>
2016-02-26 13:57:50 +0800

    java classloader

---
+++ src/com/cgl/java/classLoader/ClassLoaderSysTest.java
@@ -0,0 +1,18 @@
+package com.cgl.java.classLoader;
+
+/**
+ * Created by chenguangliang on 9/15/15.
+ */
+public class ClassLoaderSysTest {
+    public static void main(String[] args) {
+//        URLClassLoader URLClassLoader = URLClassLoader
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println("SystemClass loader:" + systemClassLoader);
+
+        ClassLoader extensionLoader = systemClassLoader.getParent();
+        System.out.println("ExtensionClass loader:" + extensionLoader);
+
+        ClassLoader brostLoader = extensionLoader.getParent();
+        System.out.println("Brost loader:" + extensionLoader);
+    }
+}
